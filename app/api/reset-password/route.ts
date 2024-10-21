6598e1b9ef860f6cad8a11564bbe3c96fd8a700b
charansr <charan.sridhar26@gmail.com>
2024-10-21 00:35:18 -0700

    fixed password hashing, removed console output, and use resetToken to find user

--- app/api/reset-password/route.ts
+++ app/api/reset-password/route.ts
@@ -1,32 +1,25 @@
 import { NextResponse } from 'next/server';
 import UserModel from '@/models/userModel';
-import bcrypt from 'bcrypt';
 
 
 export async function POST(request: Request) {
   const { token, newPassword } = await request.json();
 
-
   try {
-    console.log("wow")
 
     // Find the user with the reset token and check if the token is still valid
-    /*const User = await UserModel.findOne({
+    const User = await UserModel.findOne({
       resetToken: token,
       resetTokenExpiry: { $gt: Date.now() }, // Token should still be valid (not expired)
-    });*/
-
-    const User = await UserModel.findOne({ email: "charansr@aimpower.org" });
+    });
 
     if (!User) {
       return NextResponse.json({ success: false, message: 'Invalid or expired token' }, { status: 400 });
     }
 
-    // Hash the new password
-    const hashedPassword = await bcrypt.hash(newPassword, 10);
 
     // Update the user's password and remove the reset token and expiry
-    User.password = hashedPassword;
+    User.password = newPassword;
     User.resetToken = "";
     User.resetTokenExpiry = new Date();
     await User.save();
