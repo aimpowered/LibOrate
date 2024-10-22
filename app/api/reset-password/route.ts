aa6934e50d894e5dc2ed28950c8d0e9bf6e67484
charansr <charan.sridhar26@gmail.com>
2024-10-22 00:52:17 -0700

    ran prettier

--- app/api/reset-password/route.ts
+++ app/api/reset-password/route.ts
@@ -1,20 +1,21 @@
-import { NextResponse } from 'next/server';
-import UserModel from '@/models/userModel';
-
+import { NextResponse } from "next/server";
+import UserModel from "@/models/userModel";
 
 export async function POST(request: Request) {
   const { token, newPassword } = await request.json();
 
   try {
-
     //Locate user with the right token
     const User = await UserModel.findOne({
       resetToken: token,
-      resetTokenExpiry: { $gt: Date.now() }, 
+      resetTokenExpiry: { $gt: Date.now() },
     });
 
     if (!User) {
-      return NextResponse.json({ success: false, message: 'Invalid or expired token' }, { status: 400 });
+      return NextResponse.json(
+        { success: false, message: "Invalid or expired token" },
+        { status: 400 },
+      );
     }
 
     // Update the user's password, remove the reset token, and set expiry date to current time for security
@@ -23,9 +24,15 @@ export async function POST(request: Request) {
     User.resetTokenExpiry = new Date();
     await User.save();
 
-    return NextResponse.json({ success: true, message: 'Password reset successfully' });
+    return NextResponse.json({
+      success: true,
+      message: "Password reset successfully",
+    });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ success: false, message: 'Internal Server Error' }, { status: 500 });
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 },
+    );
   }
 }
