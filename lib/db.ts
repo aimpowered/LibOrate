cc35157264b3e8e565f38dd1df89c447cb54dd8d
Ben Lickly <blickly@google.com>
2024-10-30 09:33:23 -0700

    Make db fail faster when DATABASE_URL not set

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,13 @@
 // MongoDB connection
 import mongoose from "mongoose";
 
-const url = process.env.DATABASE_URL as string;
+function getDatabaseUrl() {
+  const url = process.env.DATABASE_URL;
+  if (url == null) throw new Error('DATABASE_URL not found in environment')
+  return url;
+}
+
+const url = getDatabaseUrl();
 let connection: typeof mongoose;
 
 const startDB = async () => {
