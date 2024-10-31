7859aefe913855d5c8bcbaa9c2dd2dfac4cfebb3
Ben Lickly <blickly@google.com>
2024-10-30 19:55:05 -0700

    Make the cypress example more complete

--- cypress/e2e/login-spec.cy.ts
+++ cypress/e2e/login-spec.cy.ts
@@ -1,9 +1,22 @@
 describe('template spec', () => {
   it('passes', () => {
+    const email = 'foobar@example.com';
+    const password = 'secret';
     cy.visit('http://localhost:3000');
     cy.contains('sign up').click();
-    cy.contains('Email').click().type('foobar@example.com');
-    cy.contains('Password').click().type('secret');
+    cy.contains('Create an account');
+    cy.contains('Email').click().type(email);
+    cy.contains('Password').click().type(password);
     cy.contains('Sign Up').click();
+
+    cy.wait(5000); // TODO: make this faster and remove this line
+    cy.contains('User created successfully');
+    cy.contains('sign in').click();
+    cy.contains('Email').click().type(email);
+    cy.contains('Password').click().type(password);
+    cy.contains('Sign In').click();
+
+    // Sign in successful (reach name tag page)
+    cy.contains('Preferred Name');
   })
 })
