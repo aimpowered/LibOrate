7ec309a3aeb769316d72bbf7f19a31c136f48f69
Ben Lickly <blickly@google.com>
2024-11-02 14:48:04 -0700

    Make cypress viewport dimensions more realistic

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,12 @@
-export default {
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  viewportWidth: 400,
+  viewportHeight: 900,
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+});
