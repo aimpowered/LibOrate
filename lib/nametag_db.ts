3ceb908ed1eba9d3aa850ce25995382c587bc314
Ben Lickly <blickly@google.com>
2024-11-02 09:27:48 -0700

    Refactor nametag_db API to use promise exceptionso
    This is more flexible than logging to the console

--- lib/nametag_db.ts
+++ lib/nametag_db.ts
@@ -1,44 +1,34 @@
 import { NameTagContent } from "@/components/NameTagForm";
 import { getSession } from "next-auth/react";
 
-export async function fetchNametagFromDB(): Promise<
-  NameTagContent | undefined
-> {
-  const session = await getSession();
-
-  if (!session || !session.user) {
-    console.error("User session is not defined.");
-    return;
+export async function fetchNametagFromDB(): Promise<NameTagContent> {
+  const user = await getUser();
+  const res = await fetch(
+    "/api/auth/users/userData/nameTag?userEmail=" + user.email,
+    { method: "GET" },
+  );
+  const json = await res.json();
+  if (json.success && json.nameTag) {
+    return json.nameTag;
   }
-
-  try {
-    const res = await fetch(
-      "/api/auth/users/userData/nameTag?userEmail=" + session.user.email,
-      { method: "GET" },
-    )
-    const json = await res.json();
-    if (json.success && json.nameTag) {
-      return json.nameTag;
-    }
-    console.log("No nametag stored in DB.");
-  } catch (e) {
-      console.error(e);
-  }
+  throw new Error("No nametag stored in DB.");
 }
 
 export async function updateNameTagInDB(newNameTag: NameTagContent) {
-  const session = await getSession();
-
-  if (!session || !session.user) {
-    console.error("User session is not defined.");
-    return;
-  }
-
+  const user = await getUser();
   return fetch("/api/auth/users/userData/nameTag", {
       method: "POST",
       body: JSON.stringify({
-        email: session.user.email,
+        email: user.email,
         nameTag: newNameTag,
       }),
   });
+}
+
+async function getUser() {
+  const session = await getSession();
+  if (!session || !session.user) {
+    throw new Error("User session is not defined.");
+  }
+  return session.user;
 }
