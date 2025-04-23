3011818a31609d44496afceb12262c8161a5c4a0
harry <harry>
2025-04-24 02:03:46 +0800

    customize handwave buttons

--- components/WaveHandPicker.tsx
+++ components/WaveHandPicker.tsx
@@ -1,26 +1,35 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { WaveHandButton } from "@/components/WaveHandButton";
+import { AddHandButton } from "@/components/AddWaveHandButton";
 import { HandWaveBadge } from "@/lib/draw_badge_api";
+import { RetryError } from "@/components/RetryError";
 
 import "@/app/css/Wavehand.css";
-
-const waveHandEmoji = "👋";
 
 interface WaveHandPickerProps {
   initialHands: string[];
   updateHandWaveBadge: (badge: HandWaveBadge) => void;
+  hasError: boolean;
+  onRetry: () => void;
+  onAdd: (text: string) => void;
+  onDelete: (text: string) => void;
 }
 
 export function WaveHandPicker({
   initialHands,
   updateHandWaveBadge,
+  hasError,
+  onRetry,
+  onAdd,
+  onDelete,
 }: WaveHandPickerProps) {
   const [selectedButtonId, setSelectedButtonId] = useState(-1);
+  const [hands, setHands] = useState(initialHands);
 
-  const appendEmoji = (text: string) => {
-    return waveHandEmoji + " " + text;
-  };
+  useEffect(() => {
+    setHands(initialHands);
+  }, [initialHands]);
 
   const clickButton = (id: number, text: string) => {
     if (selectedButtonId == id) {
@@ -32,21 +41,45 @@ export function WaveHandPicker({
       // draw selected HandWaveBadge
       updateHandWaveBadge({
         visible: true,
-        waveText: appendEmoji(text),
+        waveText: text,
       });
     }
   };
 
+  const addNewHand = (text: string) => {
+    if (text && text.trim() !== "") {
+      setHands([...hands, text.trim()]);
+      onAdd(text.trim());
+    }
+  };
+
+  const deleteHand = (text: string) => {
+    setHands(hands.filter((hand) => hand !== text));
+    onDelete(text);
+  };
+
   return (
     <div className="button-rows">
-      {initialHands.map((text, index) => (
-        <WaveHandButton
-          key={text}
-          selected={index == selectedButtonId}
-          onClick={() => clickButton(index, text)}
-          text={appendEmoji(text)}
+      {hasError ? (
+        <RetryError
+          onRetry={onRetry}
+          className="wavehand-fetch-error"
+          buttonClassName="wavehand-retry-button"
         />
-      ))}
+      ) : (
+        <>
+          {hands.map((text, index) => (
+            <WaveHandButton
+              key={text}
+              selected={index == selectedButtonId}
+              onClick={() => clickButton(index, text)}
+              onDelete={deleteHand}
+              text={text}
+            />
+          ))}
+          <AddHandButton onAdd={addNewHand} />
+        </>
+      )}
     </div>
   );
 }
