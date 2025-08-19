89b3e1b3f90dc6415b89a410de9cb3092f0704a3
Ben Lickly <blickly@aimpower.org>
2025-08-19 19:02:25 +0200

    Run prettier:format

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,8 +1,8 @@
-import { Button as HeadlessButton } from '@headlessui/react';
-import clsx from 'clsx';
-import React from 'react';
+import { Button as HeadlessButton } from "@headlessui/react";
+import clsx from "clsx";
+import React from "react";
 
-interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
   selected?: boolean;
 }
 
@@ -12,20 +12,20 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <HeadlessButton
         ref={ref}
         className={clsx(
-          'bg-transparent text-black border border-gray-300 rounded-full cursor-pointer m-1 text-xl shadow-[0_2px_1px_white] hover:bg-muted',
+          "bg-transparent text-black border border-gray-300 rounded-full cursor-pointer m-1 text-xl shadow-[0_2px_1px_white] hover:bg-muted",
           {
-            'bg-destructive text-destructive-foreground font-bold': selected,
+            "bg-destructive text-destructive-foreground font-bold": selected,
           },
-          className
+          className,
         )}
         {...props}
       >
         {children}
       </HeadlessButton>
     );
-  }
+  },
 );
 
-Button.displayName = 'Button';
+Button.displayName = "Button";
 
 export { Button };
